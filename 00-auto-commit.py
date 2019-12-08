ceb8d529c325b6246cbcd4ee70cfbe8aae97077a
xiangshuijiao <2251664811@qq.com>
2019-12-08 23:58:17 +0800

    2019-12-08 23:58:17

--- 00-auto-commit.py
+++ 00-auto-commit.py
@@ -8,5 +8,5 @@ def timer(n):
         os.system("git add --all && git commit -m  \"" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) +  "\" && git push origin master")
         time.sleep(n)
 
-timer(1)
+timer(180)
     
