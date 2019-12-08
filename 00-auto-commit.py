9f69125d6a70485acdd21e1ff358611a36fd618b
xiangshuijiao <2251664811@qq.com>
2019-12-09 00:10:05 +0800

    2019-12-09 00:10:05

--- 00-auto-commit.py
+++ 00-auto-commit.py
@@ -6,6 +6,7 @@ from subprocess import run
 def timer(n):
     while True:
         run("git add --all && git commit -m  \"" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) +  "\" && git push origin master", shell=True)
+        print("start sleep...\n")
         time.sleep(n)
 timer(180)
     
