e51c90162e23546213496cfd9d2b98fcd28503c0
xiangshuijiao <2251664811@qq.com>
2020-10-12 10:45:10 +0800

    Create make.sh

---
+++ make.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [[ "$2" == "" ]]
+then
+	make_compile_options="MODEL=$1"
+else
+	make_compile_options="MODEL=$1 SPEC=$2"
+fi
+
+rm nohup.out && touch nohup.out && tail -f nohup.out&
+nohup make $make_compile_options env_build 
+nohup make $make_compile_options boot_build 
+nohup make $make_compile_options kernel_build 
+nohup make $make_compile_options modules_build 
+nohup make $make_compile_options apps_build 
+nohup make $make_compile_options cmm -B 
+nohup make $make_compile_options fs_build 
+nohup make $make_compile_options image_build 
+
