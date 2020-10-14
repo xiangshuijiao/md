289249641c6b07bbd7702be1020837e93339bd79
xiangshuijiao <2251664811@qq.com>
2020-10-14 23:23:51 +0800

    2020年10月14日 23:23:51

--- commit.sh
+++ commit.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+# 给参数指定的markdown文件生成目录并add commit push
 export PATH
+
+# 准备工作
+echo "git pull -f" && git pull -f
+generated_TOC_directory="generated_TOC_directory.md"
+A_temporary_file_composed_of_directories_and_text="A_temporary_file_composed_of_directories_and_text.md"
 
 # 检查参数是否存在
 if [ "$1" = "" ]
@@ -19,7 +25,6 @@ do
 		break
 	fi
 done < $1
-
 if [ "$bool" == true ]
 then
 echo "成功匹配自定义分隔符"
@@ -43,40 +48,40 @@ do
 done < $1
 
 
-# 临时文件“生成的TOC目录.md”已经存在则删除
-if [ -f "生成的TOC目录.md" ]
+# 临时文件 $generated_TOC_directory 已经存在则删除
+if [ -f "$generated_TOC_directory" ]
 then
-	rm 生成的TOC目录.md
-	echo "清理旧的文件“生成的TOC目录.md”"
+	rm $generated_TOC_directory
+	echo 清理旧的文件$generated_TOC_directory
 else
-	echo “旧的文件“生成的TOC目录.md”不存在，所以不需要清理”
+	echo 旧的文件$generated_TOC_directory不存在，所以不需要清理
 fi
 
 
-# 临时文件“ 01-ToBeModifide-merge.md”已经存在则删除
-if [ -f " 01-ToBeModifide-merge.md" ]
+# 临时文件 $A_temporary_file_composed_of_directories_and_text 已经存在则删除
+if [ -f "$A_temporary_file_composed_of_directories_and_text" ]
 then
-        rm  01-ToBeModifide-merge.md
-        echo "清理旧的文件“ 01-ToBeModifide-merge.md”"
+        rm  $A_temporary_file_composed_of_directories_and_text
+        echo 清理旧的文件 $A_temporary_file_composed_of_directories_and_text
 else
-        echo “旧的文件“ 01-ToBeModifide-merge.md”不存在，所以不需要清理”
+        echo 旧的文件 $A_temporary_file_composed_of_directories_and_text 不存在，所以不需要清理
 fi
 
 
 # 生成目录
-./00-gh-md-toc.exe $1 > 生成的TOC目录.md
+./00-gh-md-toc.exe $1 > $generated_TOC_directory
 
 
-# 删除目录前3行对应的一级标题“Table of Contents”
-sed -i '1,3d' 生成的TOC目录.md
-cat 生成的TOC目录.md $1 > 01-ToBeModifide-merge.md
+# 删除目录前3行对应的一级标题 Table of Contents
+sed -i '1,3d' $generated_TOC_directory
+cat $generated_TOC_directory $1 > $A_temporary_file_composed_of_directories_and_text
 
 
-# 合并后的文件“01-ToBeModifide-merge.md”如果已经成功生成则删除合并前的文件，且修改名字为合并前的文件
-if [ -f "01-ToBeModifide-merge.md" ]
+# 合并后的文件 $A_temporary_file_composed_of_directories_and_text 如果已经成功生成则删除合并前的文件，且修改名字为合并前的文件
+if [ -f "$A_temporary_file_composed_of_directories_and_text" ]
 then
-	rm $1 生成的TOC目录.md
-	mv 01-ToBeModifide-merge.md $1
+	rm $1 $generated_TOC_directory
+	mv $A_temporary_file_composed_of_directories_and_text $1
 	echo "成功生成TOC目录并合并到文件开头"
 else
 	echo "文件合并失败"
