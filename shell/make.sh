684f1eb6ac04126e8dc6b41e940f807d140d6b59
xiangshuijiao <2251664811@qq.com>
2020-10-15 09:55:30 +0800

    Update make.sh

--- shell/make.sh
+++ shell/make.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 if [[ "$2" == "" ]]
 then
-	make_compile_options="MODEL=$1"
+  make_compile_options="MODEL=$1"
 else
-	make_compile_options="MODEL=$1 SPEC=$2"
+  make_compile_options="MODEL=$1 SPEC=$2"
 fi
 
 rm -rf nohup.out && touch nohup.out && tail -f nohup.out&
-nohup make $make_compile_options env_build 
-nohup make $make_compile_options boot_build 
-nohup make $make_compile_options kernel_build 
-nohup make $make_compile_options modules_build 
-nohup make $make_compile_options apps_build 
-nohup make $make_compile_options cmm -B 
-nohup make $make_compile_options fs_build 
-nohup make $make_compile_options image_build 
-
-
+nohup make $make_compile_options env_build && if [ $? -ne 0 ]; then echo "failed"; exit -1; else echo "succeed"; fi 
+nohup make $make_compile_options boot_build  && if [ $? -ne 0 ]; then echo "failed"; exit -1; else echo "succeed"; fi
+nohup make $make_compile_options kernel_build  && if [ $? -ne 0 ]; then echo "failed"; exit -1; else echo "succeed"; fi
+nohup make $make_compile_options modules_build  && if [ $? -ne 0 ]; then echo "failed"; exit -1; else echo "succeed"; fi
+nohup make $make_compile_options apps_build  && if [ $? -ne 0 ]; then echo "failed"; exit -1; else echo "succeed"; fi
+nohup make $make_compile_options cmm -B  && if [ $? -ne 0 ]; then echo "failed"; exit -1; else echo "succeed"; fi
+nohup make $make_compile_options fs_build  && if [ $? -ne 0 ]; then echo "failed"; exit -1; else echo "succeed"; fi
+nohup make $make_compile_options image_build  && if [ $? -ne 0 ]; then echo "failed"; exit -1; else echo "succeed"; fi
