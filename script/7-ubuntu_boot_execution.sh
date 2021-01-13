6843946ae883bd8b84e24e71dc5bf8afbae599de
xiangshuijiao <2251664811@qq.com>
2021-01-13 16:36:15 +0800

    Update 7-ubuntu_boot_execution.sh

--- script/7-ubuntu_boot_execution.sh
+++ script/7-ubuntu_boot_execution.sh
@@ -7,24 +7,41 @@ docker start opengrok
 docker start gitlab
 docker start docker1
 
+# 抓包网卡重命名
+rt5572_old_name=wlx6466b31cf75b
+rt5572_new_name=wifi_rt5572
 /etc/init.d/network-manager stop
-
-ifconfig wlx14cf92cc2987 down
+ifconfig $rt5572_old_name down
 rfkill unblock wifi  # 避免SIOCSIFFLAGS: 由于 RF-kill 而无法操作
-ip link set wlx14cf92cc2987 name wlan_new # 网卡重命名
-iwconfig wlan_new mode Monitor
-ifconfig wlan_new up
+ip link set $rt5572_old_name name $rt5572_new_name # 网卡重命名
+iwconfig $rt5572_new_name mode Monitor
+ifconfig $rt5572_new_name up
+ifconfig $rt5572_new_name down
 
-ifconfig wlx6466b31cf75b down
-rfkill unblock wifi  # 避免SIOCSIFFLAGS: 由于 RF-kill 而无法操作
-ip link set wlx6466b31cf75b name wlan_old # 网卡重命名
-iwconfig wlan_old mode Monitor
-ifconfig wlan_old up
-
-
+count=0
 while :
 do
     chmod -R 777 /home/bba/
     chmod -R 777 /home/share_data_folder/
+
+    # 当抓包接口up且wireshark进程连续5分钟不存在也即没有在抓包时就down掉抓包接口，避免抓包网卡过度工作
+    if [ `ifconfig | grep $rt5572_new_name | wc -l` -ne 0 ]
+    then
+        if [ `ps -aux | grep wireshark | grep -v "grep" | wc -l` -ne 0 ]
+        then 
+            count=0
+        else
+            count=`expr $count + 1` 
+        fi
+
+        if [ $count -gt 5 ]
+        then   
+            ifconfig $rt5572_new_name down
+            count=0
+        fi
+    else
+        count=0
+    fi
+
     sleep 60
 done
