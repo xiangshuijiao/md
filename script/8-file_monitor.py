8a0672024330f4fecfc72fcf6c2a197e35d0bac6
xiangshuijiao <2251664811@qq.com>
2021-01-26 20:49:48 +0800

    Update 8-file_monitor.py

--- script/8-file_monitor.py
+++ script/8-file_monitor.py
@@ -3,11 +3,13 @@ import os
 import time
 from playsound import playsound
 from tkinter import filedialog
- 
+from ctypes import *
+
+user32 = windll.LoadLibrary('user32.dll') 
 root = tk.Tk()
 root.withdraw()
 
-# 17秒内选择的文件大小没有发生变化就播放指定音乐
+# 17秒内选择的文件大小没有发生变化就***
 file_path = filedialog.askopenfilename()
 count = 0
 file_size_past = 0
@@ -21,7 +23,8 @@ while (True):
         count = count + 1
     
     if count == 17 :
-        playsound('file://debugpc.jkn/share_data_folder/4-script/1.mp3')
+        # playsound('file:///D:/Music/1.mp3') # 播放指定音乐
+        user32.LockWorkStation() # windows锁屏
         break
 
     time.sleep(1)
