9ed678dc549d1cd4e364b545506eb35be7d62e93
xiangshuijiao <2251664811@qq.com>
2021-01-26 20:50:03 +0800

    Update 9-sniffer.sh

--- script/9-sniffer.sh
+++ script/9-sniffer.sh
@@ -9,7 +9,7 @@ echo 请输入要抓包的信道, 不抓包则指定0信道...
 exit -1
 fi
 
-# 拉起没有up的抓包接口
+# 抓包接口没有up时up抓包接口
 if [ `ifconfig | grep $rt5572_new_name | wc -l` -eq 0 ]
 then
     ifconfig $rt5572_new_name up
@@ -25,4 +25,8 @@ else
     ifconfig $rt5572_new_name down
 fi
 
-
+# wireshark没有启动时启动wireshark
+if [ `ps -aux | grep wireshark | grep -v "grep" | wc -l` -eq 0 ]
+then
+    wireshark&
+fi
